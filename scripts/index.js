2cf3b5aa547bab15bbb1711a13a96db4a3f3dfe2
Misael Corvera <mcmalfaro@hotmail.com>
2019-06-12 18:33:30 -0700

    getting results done

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,8 @@ let key = "b95d89a6ee414e39926181755191803"
 const input = document.getElementById("inputBar");
 const btn = document.getElementById("search");
 const locBtn = document.getElementById("getLoc")
+let errorTxt = document.getElementById("error");
+           
 
 //assigning event
 btn.addEventListener('click', search)
@@ -19,33 +21,60 @@ input.addEventListener('keyup',function(e){
 function search(){
     let city = input.value
     const currentType = "forecast" // use for fetch
-    let search = `${url}/${currentType}.json?key=${key}&q=${city}`
+    let search = `${url}/${currentType}.json?key=${key}&q=${city}&days=6`
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myArr = JSON.parse(this.responseText);
-            myFunction(myArr);
+            myResult = JSON.parse(this.responseText);
+            DisplayResult(myResult);
+
         }
         else if(this.status == 400){
-            console.log("sorry")
-            var out = '<h2 id="err">Error... Please try again</h2>';
-            document.getElementById("id01").innerHTML = out;
+           errorTxt.style.visibility = "visible"
         }
                
     };
     xmlhttp.open("GET", search, true);
     xmlhttp.send();
+}
+
+function DisplayResult(res) {
+    console.log(res)
+    errorTxt.style.visibility = "hidden";
+    currentDisplay(res);
+    forecastDisplay(res);
+}
+
+function currentDisplay(res){
+    let city = document.getElementById("city");
+    let logo = document.getElementById("logo");
+    let temp = document.getElementById("temp");
+    let cond = document.getElementById("cond");
+    let feels = document.getElementById("feels");
+
+    city.innerText = `${res.location.name}  ${res.location.region}, ${res.location.country}`
+    logo.src = res.current.condition.icon;
+    temp.innerText = `${res.current.temp_f}°`;
+    cond.innerText = `${res.current.condition.text}`
+    feels.innerHTML = `Feels like: ${res.current.feelslike_f}°`
+
+}
+
+function forecastDisplay(res){
+    let days = res.forecast.forecastday;
+    let forecast = document.getElementById("forecast");
     
-    function myFunction(arr) {
-        console.log(arr)
-        let out = ""
-        out = `<h1 id="err">${arr.location.name}</h1>`;
-        document.getElementById("id01").innerHTML = out;
-        
-        
-        
+    
+    for(let i = 1; i < days.length; i++){
+        let logo = `<div class="box"><img id="logoFore" class="sunForecast" src="${days[i].day.condition.icon}">`;
+        let date = `<div class="detail"><h3 id="dateForecast" class="dateForecast">${days[i].date}</h3>`;
+        let text = ` <h2 id="condForecast" class="condForecast">${days[i].day.condition.text}</h2>`;
+        let max = `<h3 id="maxTemp" class="tempForecast">Max: ${days[i].day.maxtemp_f}°</h3>`;
+        let min = `<h3 id="minTemp" class="tempForecast">Min: ${days[i].day.mintemp_f}°</h3> </div></div>`;
+        forecast.innerHTML += logo + date + text + max + min;
     }
 
 }
+
 function getLocation(){
     console.log("Here you will get the user location")
     let temp = document.getElementById("err")
